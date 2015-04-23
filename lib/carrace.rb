df152bc1d225a7bb59dbda1773090f7531fb9ecd
Chris Reeve <paradigmsareconstructed@gmail.com>
2015-04-23 07:28:05 -0700

    All tests passing

--- lib/carrace.rb
+++ lib/carrace.rb
@@ -1,7 +1,41 @@
 class Car
+	attr_reader :speed
 
+	def initialize
+		@speed = 0
+	end
+
+	def accelerate(gain)
+		@speed += gain
+	end
 end
 
 class Race
+	attr_reader :cars
 
+	def initialize
+		@cars = []
+		go!
+	end
+
+	def winner
+		@cars.sort_by(&:speed).last
+	end
+
+	def loser
+		@cars.sort_by(&:speed).first
+	end
+
+private
+	def random_speed
+		(rand * 100).round
+	end
+
+	def go!
+		@cars << Car.new
+		@cars << Car.new
+
+		@cars.first.accelerate(random_speed)
+		@cars.last.accelerate(random_speed)
+	end
 end
--- spec/carrace_spec.rb
+++ spec/carrace_spec.rb
@@ -23,3 +23,44 @@
 
 require 'spec_helper'
 
+describe Race do
+	# Same as car = Car.new
+	let(:race) { Race.new }
+
+	describe '#instantiate' do
+		it "instantiates two cars" do
+			expect( race.cars.length == 2 ).to be_truthy
+		end
+
+		it "instantiates the cars to two random speeds" do
+			expect( race.cars[0].speed != race.cars[1].speed ).to be_truthy
+		end
+	end
+
+	describe '#go!' do
+		it "has a winner" do
+			expect( race.winner ).to be_truthy
+		end
+
+		it "has a loser" do
+			expect( race.loser ).to be_truthy
+		end
+	end
+
+end
+
+describe Car do
+	# Same as race = Race.new
+	let(:car) { Car.new }
+
+	describe '#accelerate' do
+		before do
+			car.accelerate(20)
+		end
+
+		it "can accelerate to a certain speed" do
+			expect{ car.accelerate(20) }.to change{car.speed}.by(20)
+		end
+	end
+
+end
